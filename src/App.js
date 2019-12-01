32d64f8e2572f045a1d1d459203b87cc29793d01
utkugithub <55439867+utkugithub@users.noreply.github.com>
2019-12-01 21:41:28 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,14 +11,7 @@ class App extends Component {
           <p>
             Coming (very) soon ! :)
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          
         </header>
       </div>
     );
