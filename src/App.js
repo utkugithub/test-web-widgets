b851fbb255306e07a04eaf83cc081cc0f6ea3014
utkugithub <55439867+utkugithub@users.noreply.github.com>
2020-05-08 10:49:51 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+    componentDidMount () {
+    // Include the Crisp code here, without the <script></script> tags
+    window.$crisp=[];
+    window.CRISP_WEBSITE_ID="2b617e32-969e-4b91-9e68-dc1a981724e3";
+    (function(){d=document;s=d.createElement("script");
+    s.src="https://client.crisp.chat/l.js";
+    s.async=1;
+    d.getElementsByTagName("head")[0].appendChild(s);
+    })(); 
+  
+  };
+  
   render() {
     return (
       <div className="App">
