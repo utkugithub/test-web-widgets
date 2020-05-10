ab7e49b6a2cdc6c05cfc59aa54c8289862269ff5
utkugithub <55439867+utkugithub@users.noreply.github.com>
2020-05-10 09:50:53 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,18 +4,6 @@ import './App.css';
 
 class App extends Component {
   
-    componentDidMount () {
-    // Include the Crisp code here, without the <script></script> tags
-    window.$crisp=[];
-      window.CRISP_WEBSITE_ID="a19b5940-724e-4892-aa89-545ea5c6958b";
-      (function(){d=document;
-                  s=d.createElement("script");
-                  s.src="https://client.crisp.chat/l.js";
-                  s.async=1;
-                  d.getElementsByTagName("head")[0].appendChild(s);
-     })();
-  };
-  
   render() {
     return (
       <div className="App">
